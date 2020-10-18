a1345afbc8c8d110a2d76b955b2b526c628628d5
daesungra <daesungra@gmail.com>
2020-10-19 00:44:33 +0900

    config: Add default error pages

--- app/__init__.py
+++ app/__init__.py
@@ -4,22 +4,48 @@ monkey.patch_all()
 import logging
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 
+# import config  # TODO: Create default config file
 from app.api import main
 
 LOGGER = logging.getLogger(__name__)
 
 
 def create_app() -> Flask:
-    flask_app = Flask(__name__)
-    register_blueprints(flask_app)
-    flask_app.config['MAX_CONTENT_LENGTH'] = 1 << 40
-    flask_app.secret_key = os.urandom(12)
+    app = Flask(__name__)
+
+    set_logger()
+    register_blueprints(app)
+    register_error_pages(app)
+    app.config['MAX_CONTENT_LENGTH'] = 1 << 40
+    app.config['SECRET_KEY'] = os.urandom(12)
+    # app.config['PERMANENT_SESSION_LIFETIME'] = config.session_config['permanent_session_lifetime']
+    
+    CORS(app)
 
     LOGGER.info('Setting done..')
-    return flask_app
+    return app
 
 
-def register_blueprints(flask_app: Flask):
-    flask_app.register_blueprint(main.API)
+def set_logger():
+    pass
+
+
+def register_blueprints(app: Flask):
+    app.register_blueprint(main.API)
+
+
+def register_error_pages(app: Flask):
+    @app.errorhandler(404)
+    def page_not_found(error):
+        LOGGER.error(error)
+        return render_template('error/404.html'), 200
+
+
+    @app.errorhandler(500)
+    def server_side_error(error):
+        LOGGER.error(error)
+        return render_template('error/500.html'), 200
+
--- run_app.sh
+++ run_app.sh
@@ -4,5 +4,5 @@ export PYTHONPATH="$PJT_DIR"
 cd "$PJT_DIR"
 
 . ./venv/bin/activate
-gunicorn --workers 1 --threads 8 --worker-class gevent --timeout 30 --name flask_app app.server:app
+gunicorn --workers 1 --threads 8 --worker-class gevent --timeout 30 --name app app.server:app
 
