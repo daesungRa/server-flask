f3834377734d3a1bd96646bd808e2a92f49ef602
daesungra <daesungra@gmail.com>
2020-10-19 18:00:51 +0900

    config: Add root logger

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ monkey.patch_all()
 import logging
 import os
 
+from pathlib import Path
+from logging import Formatter
+from logging.handlers import TimedRotatingFileHandler
 from flask import Flask, render_template
 from flask_cors import CORS
 
@@ -25,12 +28,42 @@ def create_app() -> Flask:
     
     CORS(app)
 
-    LOGGER.info('Setting done..')
+    LOGGER.info('Application setting done..')
     return app
 
 
 def set_logger():
-    pass
+    fmt = Formatter(
+        '[%(levelname)s %(asctime)s %(filename)s:%(lineno)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
+
+    root_logger = logging.getLogger()
+    for handle in root_logger.handlers:
+        root_logger.removeHandler(handle)
+
+    # Add stream handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(fmt)
+    stream_handler.setLevel(logging.INFO)
+    root_logger.addHandler(stream_handler)
+
+    # Add timed file handler
+    project_root = Path(__file__).resolve().parent.parent
+    log_dir = project_root / 'logs'
+    log_dir.mkdir(exist_ok=True)
+    file_handler = TimedRotatingFileHandler(
+        filename=log_dir / 'app.log',
+        when='midnight',
+        interval=1,
+        backupCount=100,
+        encoding='UTF-8',
+    )
+    file_handler.suffix = '%Y-%m-%d'
+    file_handler.setFormatter(fmt)
+    file_handler.setLevel(logging.INFO)
+    root_logger.addHandler(file_handler)
+    root_logger.setLevel(logging.INFO)
 
 
 def register_blueprints(app: Flask):
